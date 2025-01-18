4af9c90d38f7696a23ee14aeeb8181820998d88c
SANGYU <sangyu.li@outlook.com>
2025-01-18 09:38:16 +0800

    添加项目文件。

---
+++ BallBallYou/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace BallBallYou
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_CountGo = new System.Windows.Forms.Label();
+            this.labelBack = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 20F);
+            this.groupBox1.Location = new System.Drawing.Point(67, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "球卡信息";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("宋体", 20F);
+            this.radioButton3.Location = new System.Drawing.Point(303, 45);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(122, 44);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "领用";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("宋体", 20F);
+            this.radioButton2.Location = new System.Drawing.Point(165, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(122, 44);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "回收";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("宋体", 20F);
+            this.radioButton1.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.radioButton1.Location = new System.Drawing.Point(37, 45);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(122, 44);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "发放";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 40F);
+            this.textBox1.Location = new System.Drawing.Point(541, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(542, 99);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("宋体", 20F);
+            this.dateTimePicker1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(193, 708);
+            this.dateTimePicker1.MinDate = new System.DateTime(1999, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(299, 42);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 715);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "按日期查询：";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 221);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(1009, 436);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(691, 722);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "发放个数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(904, 722);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "回收个数：";
+            // 
+            // label_CountGo
+            // 
+            this.label_CountGo.AutoSize = true;
+            this.label_CountGo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_CountGo.Font = new System.Drawing.Font("宋体", 30F);
+            this.label_CountGo.ForeColor = System.Drawing.Color.Green;
+            this.label_CountGo.Location = new System.Drawing.Point(794, 698);
+            this.label_CountGo.Margin = new System.Windows.Forms.Padding(3, 0, 3, 38);
+            this.label_CountGo.Name = "label_CountGo";
+            this.label_CountGo.Size = new System.Drawing.Size(57, 62);
+            this.label_CountGo.TabIndex = 5;
+            this.label_CountGo.Text = "0";
+            // 
+            // labelBack
+            // 
+            this.labelBack.AutoSize = true;
+            this.labelBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelBack.Font = new System.Drawing.Font("宋体", 30F);
+            this.labelBack.ForeColor = System.Drawing.Color.Red;
+            this.labelBack.Location = new System.Drawing.Point(1007, 698);
+            this.labelBack.Margin = new System.Windows.Forms.Padding(3, 0, 38, 38);
+            this.labelBack.Name = "labelBack";
+            this.labelBack.Size = new System.Drawing.Size(57, 62);
+            this.labelBack.TabIndex = 5;
+            this.labelBack.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 764);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "T";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1129, 846);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelBack);
+            this.Controls.Add(this.label_CountGo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "试风球卡领用 2025";
+            this.TopMost = true;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_CountGo;
+        private System.Windows.Forms.Label labelBack;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BallBallYou/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Data.Entity.Core.Metadata.Edm;
+
+namespace BallBallYou
+{
+    public partial class Form1 : Form
+    {
+        string RadioType = "发放";//默认发球
+        private SQLiteConnection sqliteConnection;
+
+        private void CreateDatabaseAndTable()
+        {
+            try
+            {
+                // 检查数据库文件是否存在，如果不存在则创建
+                if (!System.IO.File.Exists("BallRecords.db"))
+                {
+                    SQLiteConnection.CreateFile("BallRecords.db");
+                }
+
+                // 初始化并打开数据库连接
+                sqliteConnection = new SQLiteConnection("Data Source=BallRecords.db;Version=3;");
+                sqliteConnection.Open();
+
+                // 创建BallRecords表，如果不存在
+                string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS BallRecords (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    readDate TEXT NOT NULL,
+                    readTime TEXT NOT NULL,
+                    readType TEXT NOT NULL,
+                    BallsCode TEXT NOT NULL
+                )";
+
+                SQLiteCommand command = new SQLiteCommand(createTableQuery, sqliteConnection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                sqliteConnection.Close();
+            }
+        }
+
+        // 从数据库加载数据到DataGridView
+        private void LoadData()
+        {
+            // 插入数据后重新加载数据
+            try
+            {
+                if (sqliteConnection.State != ConnectionState.Open)
+                {
+                    sqliteConnection.Open();
+                }
+
+                // 获取选择的日期
+                string selectedDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+                // 查询选定日期的记录
+                string query = "SELECT readDate, readTime, readType, BallsCode FROM BallRecords WHERE readDate = @selectedDate ORDER BY readTime DESC";
+                SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(query, sqliteConnection);
+                dataAdapter.SelectCommand.Parameters.AddWithValue("@selectedDate", selectedDate);
+
+                // 使用DataTable来存储查询结果
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                // 将DataTable绑定到DataGridView
+                dataGridView1.DataSource = dataTable;
+                // 设置列标题
+                dataGridView1.Columns["readDate"].HeaderText = "读取日期";
+                dataGridView1.Columns["readTime"].HeaderText = "读取时间";
+                dataGridView1.Columns["readType"].HeaderText = "类型";
+                dataGridView1.Columns["BallsCode"].HeaderText = "球卡编码";
+                label_CountGo.Text = dataTable.Select("readType = '发放'").Length.ToString();
+                labelBack.Text = dataTable.Select("readType = '回收'").Length.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                sqliteConnection.Close();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 连接到SQLite数据库
+            string connectionString = "Data Source=BallRecords.db;Version=3;";
+            sqliteConnection = new SQLiteConnection(connectionString);
+            CreateDatabaseAndTable();
+            // 加载数据到DataGridView
+            LoadData();
+            radioButton1.Checked = true;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            else 
+            {
+                string ballsCode = textBox1.Text;
+                if (RadioType != "领用")
+                {
+                    if (sqliteConnection.State != ConnectionState.Open)
+                    {
+                        sqliteConnection.Open();
+                    }         
+                    // 查询该球卡编码在今天是否已经发放
+                    string query = "SELECT COUNT(*) FROM BallRecords WHERE BallsCode = @ballsCode";
+                    SQLiteCommand command = new SQLiteCommand(query, sqliteConnection);
+                    command.Parameters.AddWithValue("@ballsCode", ballsCode);
+
+                    // 执行查询并检查结果
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+                    if (count > 0)
+                    {
+                        // 如果记录数大于0，说明已经发放/回收/领用过
+                    }
+                    else
+                    {
+                        MessageBox.Show("该球卡未领用");
+                        textBox1.Clear();
+                        return;
+                    }
+
+                }
+                //查今天是否领用了
+                if (IsAlreadyIssuedToday(ballsCode, RadioType))
+                {
+
+                    MessageBox.Show("该球卡今天已发放或已回收，或领用未发放。");
+                    textBox1.Clear();
+                    return;
+                }
+                else
+                {
+                    string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+                    string currentTime = DateTime.Now.ToString("HH:mm:ss");
+
+                    AddRecord(currentDate, currentTime, RadioType, ballsCode);
+                }
+                textBox1.Clear();
+            }
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                RadioType = "回收";
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                RadioType = "发放";
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                RadioType = "领用";
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void AddRecord(string readDate, string readTime, string readType, string ballsCode)
+        {
+            try
+            {
+                if (sqliteConnection.State != ConnectionState.Open)
+                {
+                    sqliteConnection.Open();
+                }
+
+                string insertQuery = "INSERT INTO BallRecords (readDate, readTime, readType, BallsCode) VALUES (@readDate, @readTime, @readType, @ballsCode)";
+                SQLiteCommand command = new SQLiteCommand(insertQuery, sqliteConnection);
+
+                command.Parameters.AddWithValue("@readDate", readDate);
+                command.Parameters.AddWithValue("@readTime", readTime);
+                command.Parameters.AddWithValue("@readType", readType);
+                command.Parameters.AddWithValue("@ballsCode", ballsCode);
+
+                command.ExecuteNonQuery();
+
+                // 插入数据后重新加载数据
+                try
+                {
+                    if (sqliteConnection.State != ConnectionState.Open)
+                    {
+                        sqliteConnection.Open();
+                    }
+
+                    // 获取选择的日期
+                    string selectedDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+                    // 查询选定日期的记录
+                    string query = "SELECT readDate, readTime, readType, BallsCode FROM BallRecords WHERE readDate = @selectedDate ORDER BY readTime DESC";
+                    SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(query, sqliteConnection);
+                    dataAdapter.SelectCommand.Parameters.AddWithValue("@selectedDate", selectedDate);
+
+                    // 使用DataTable来存储查询结果
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // 将DataTable绑定到DataGridView
+                    dataGridView1.DataSource = dataTable;
+                    // 设置列标题
+                    dataGridView1.Columns["readDate"].HeaderText = "读取日期";
+                    dataGridView1.Columns["readTime"].HeaderText = "读取时间";
+                    dataGridView1.Columns["readType"].HeaderText = "类型";
+                    dataGridView1.Columns["BallsCode"].HeaderText = "球卡编码";
+                    label_CountGo.Text = dataTable.Select("readType = '发放'").Length.ToString();
+                    labelBack.Text = dataTable.Select("readType = '回收'").Length.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    textBox1.Clear();
+                }
+                finally
+                {
+                    sqliteConnection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                textBox1.Clear();
+            }
+            finally
+            {
+                sqliteConnection.Close();
+            }
+        }
+
+        private bool IsAlreadyIssuedToday(string ballsCode, string readType)
+        {
+            try
+            {
+                if (sqliteConnection.State != ConnectionState.Open)
+                {
+                    sqliteConnection.Open();
+                }
+
+                // 获取当前日期
+                string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+
+                // 查询该球卡编码在今天是否已经发放
+                string query = "SELECT COUNT(*) FROM BallRecords WHERE BallsCode = @ballsCode AND readDate = @currentDate AND readType = @readType";
+                SQLiteCommand command = new SQLiteCommand(query, sqliteConnection);
+                command.Parameters.AddWithValue("@ballsCode", ballsCode);
+                command.Parameters.AddWithValue("@currentDate", currentDate);
+                command.Parameters.AddWithValue("@readType", readType);
+                
+                // 执行查询并检查结果
+                int count = Convert.ToInt32(command.ExecuteScalar());
+                return count > 0; // 如果记录数大于0，说明已经发放/回收/领用过
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                textBox1.Clear();
+                return false;
+            }
+            finally
+            {
+                sqliteConnection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(100000000, 1000000000); // 生成一个 9 位数
+            textBox1.Text = "2222222";
+            KeyEventArgs key = new KeyEventArgs(Keys.Enter);
+            textBox1_KeyDown(this, key);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqliteConnection.State != ConnectionState.Open)
+                {
+                    sqliteConnection.Open();
+                }
+
+                // 获取选择的日期
+                string selectedDate = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+
+                // 查询选定日期的记录
+                string query = "SELECT readDate, readTime, readType, BallsCode FROM BallRecords WHERE readDate = @selectedDate ORDER BY readTime DESC";
+                SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(query, sqliteConnection);
+                dataAdapter.SelectCommand.Parameters.AddWithValue("@selectedDate", selectedDate);
+
+                // 使用DataTable来存储查询结果
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                // 将DataTable绑定到DataGridView
+                dataGridView1.DataSource = dataTable;
+                // 设置列标题
+                dataGridView1.Columns["readDate"].HeaderText = "读取日期";
+                dataGridView1.Columns["readTime"].HeaderText = "读取时间";
+                dataGridView1.Columns["readType"].HeaderText = "类型";
+                dataGridView1.Columns["BallsCode"].HeaderText = "球卡编码";
+                label_CountGo.Text = dataTable.Select("readType = '发放'").Length.ToString();
+                labelBack.Text = dataTable.Select("readType = '回收'").Length.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                textBox1.Clear();
+            }
+            finally
+            {
+                sqliteConnection.Close();
+            }
+        }
+    }
+}
